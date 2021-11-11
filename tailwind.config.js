069af62ba60c381ee9894a6024c82b4169c4e5e5
Jesutooni <edeworuyoyou@gmail.com>
2021-11-11 17:34:32 +0100

    files

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        "source-sans": ["'Source Sans Pro'", "sans-serif"],
+        "dm-san": ["'DM Sans'", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          green: {
+            dark: "#224932",
+            bright: "#22B65D",
+          },
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
