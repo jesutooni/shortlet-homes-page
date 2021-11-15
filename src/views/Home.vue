0db6233461fa5c4f5dc35fd91ebbad3047ddb428
Jesutooni <edeworuyoyou@gmail.com>
2021-11-15 18:24:46 +0100

    desktop

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,130 +1,134 @@
 <template>
-  <div>
-    <div class="relative w-full pb-4/5 overflow-hidden">
-      <img
-        class="absolute w-full h-full object-cover"
-        :src="rooms[currentRoom]"
-        alt=""
-      />
-      <div
-        @click="prevRoom"
-        class="
-          w-8
-          h-8
-          flex
-          justify-center
-          items-center
-          bg-white
-          rounded-full
-          absolute
-          left-4
-          top-1/2
-          transform
-          -translate-y-1/2
-        "
-      >
-        <img class="w-2" src="../assets/icon-previous.svg" alt="" />
-      </div>
-      <div
-        @click="nextRoom"
-        class="
-          w-8
-          h-8
-          flex
-          justify-center
-          items-center
-          bg-white
-          rounded-full
-          absolute
-          top-1/2
-          right-4
-          transform
-          -translate-y-1/2
-        "
-      >
-        <img class="w-2" src="../assets/icon-next.svg" alt="" />
-      </div>
+  <div class="flex justify-center items-center md:h-screen">
+    <div class="md:flex justify-center h-auto md:shadow-2xl">
+      <div class="relative md:w-1/2 md:pb-0 pb-4/5 overflow-hidden">
+        <img
+          class="absolute w-full h-full object-cover"
+          :src="rooms[currentRoom]"
+          alt=""
+        />
+        <div
+          @click="prevRoom"
+          class="
+            cursor-pointer
+            w-8
+            h-8
+            flex
+            justify-center
+            items-center
+            bg-white
+            rounded-full
+            absolute
+            left-4
+            top-1/2
+            transform
+            -translate-y-1/2
+          "
+        >
+          <img class="w-2" src="../assets/icon-previous.svg" alt="" />
+        </div>
+        <div
+          @click="nextRoom"
+          class="
+            cursor-pointer
+            w-8
+            h-8
+            flex
+            justify-center
+            items-center
+            bg-white
+            rounded-full
+            absolute
+            top-1/2
+            right-4
+            transform
+            -translate-y-1/2
+          "
+        >
+          <img class="w-2" src="../assets/icon-next.svg" alt="" />
+        </div>
 
-      <div class="absolute bottom-3 w-full flex justify-center">
-        <div class="w-2 h-2 rounded-full mr-2 bg-white bg-opacity-90"></div>
-        <div class="w-2 h-2 rounded-full mr-2 bg-white bg-opacity-70"></div>
-        <div class="w-2 h-2 rounded-full mr-2 bg-white bg-opacity-50"></div>
-        <div class="w-2 h-2 rounded-full mr-2 bg-white bg-opacity-30"></div>
+        <div class="absolute bottom-3 w-full flex justify-center">
+          <div class="w-2 h-2 rounded-full mr-2 bg-white bg-opacity-90"></div>
+          <div class="w-2 h-2 rounded-full mr-2 bg-white bg-opacity-70"></div>
+          <div class="w-2 h-2 rounded-full mr-2 bg-white bg-opacity-50"></div>
+          <div class="w-2 h-2 rounded-full mr-2 bg-white bg-opacity-30"></div>
+        </div>
       </div>
-    </div>
-    <div class="p-7">
-      <div>
+      <div class="p-7 md:w-1/2">
         <div>
-          <h1
-            class="
-              font-source-sans
-              text-primary-green-dark text-2xl
-              mr-1
-              inline
-            "
-          >
-            107, Kolade Estate, Lekki.
-          </h1>
-          <div class="inline-flex">
-            <img src="../assets/check.png" alt="" />
+          <div>
+            <h1
+              class="
+                font-source-sans
+                text-primary-green-dark text-2xl
+                mr-1
+                inline
+              "
+            >
+              107, Kolade Estate, Lekki.
+            </h1>
+            <div class="inline-flex">
+              <img src="../assets/check.png" alt="" />
+            </div>
+          </div>
+
+          <div class="flex items-center text-sm font-dm-sans mt-3">
+            <img src="../assets/tag.png" alt="" />
+            <p class="ml-1">40,000 NGN/Day</p>
+          </div>
+          <div class="flex items-center text-sm font-dm-sans mt-3">
+            <img src="../assets/rate.png" alt="" />
+            <p class="ml-2">4.75 (98 reviews)</p>
           </div>
         </div>
 
-        <div class="flex items-center text-sm font-dm-sans mt-3">
-          <img src="../assets/tag.png" alt="" />
-          <p class="ml-1">40,000 NGN/Day</p>
+        <div class="mt-7">
+          <h1 class="font-source-sans text-2xl text-primary-green-dark">
+            Description
+          </h1>
+          <p class="font-dm-sans text-sm mt-2">
+            This property is situated on the fast developing Lagos Island area.
+            With a vivid view of the Atlantic Ocean and a mesmerizing skyline.
+          </p>
         </div>
-        <div class="flex items-center text-sm font-dm-sans mt-3">
-          <img src="../assets/rate.png" alt="" />
-          <p class="ml-2">4.75 (98 reviews)</p>
+        <div class="mt-7">
+          <h1 class="font-source-sans text-2xl text-primary-green-dark">
+            Property Details
+          </h1>
+          <ul class="text-sm font-dm-sans list-disc pl-7">
+            <li class="mt-2">3 bedroom apartment, all en suite</li>
+            <li class="mt-2">Spacious Terrace</li>
+            <li class="mt-2">Uninterrupted Electricity and WiFi</li>
+          </ul>
         </div>
-      </div>
 
-      <div class="mt-7">
-        <h1 class="font-source-sans text-2xl text-primary-green-dark">
-          Description
-        </h1>
-        <p class="font-dm-sans text-justify text-sm mt-2">
-          This property is situated on the fast developing Lagos Island area.
-          With a vivid view of the Atlantic Ocean and a mesmerizing skyline.
-        </p>
-      </div>
-      <div class="mt-7">
-        <h1 class="font-source-sans text-2xl text-primary-green-dark">
-          Property Details
-        </h1>
-        <ul class="text-sm font-dm-sans list-disc pl-7">
-          <li class="mt-2">3 bedroom apartment, all en suite</li>
-          <li class="mt-2">Spacious Terrace</li>
-          <li class="mt-2">Uninterrupted Electricity and WiFi</li>
-        </ul>
-      </div>
-
-      <div class="mt-10 flex items-center font-dm-sans">
-        <button
-          class="
-            bg-primary-green-dark
-            text-white
-            font-bold
-            p-5
-            w-full
-            rounded-lg
-            focus:outline-none
-          "
-        >
-          Book
-        </button>
-        <button
-          class="
-            text-primary-green-dark
-            font-bold
-            p-5
-            w-full
-            focus:outline-none
-          "
-        >
-          Contact
-        </button>
+        <div class="mt-10 flex items-center font-dm-sans">
+          <button
+            class="
+              bg-primary-green-dark
+              text-white
+              font-bold
+              p-5
+              w-full
+              rounded-lg
+              focus:outline-none
+            "
+          >
+            Book
+          </button>
+          <button
+            class="
+              text-primary-green-dark
+              font-bold
+              p-5
+              w-full
+              focus:outline-none
+            "
+          >
+            Contact
+          </button>
+        </div>
       </div>
     </div>
   </div>
